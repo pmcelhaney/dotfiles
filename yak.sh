89e0d6e71e5d0ee74d51e54983a1ac470a64b136
Patrick McElhaney <pmcelhaney@gmail.com>
2014-07-21 16:07:39 -0400

    Added bash-completion, git, and grunt to yak.sh

--- yak.sh
+++ yak.sh
@@ -1,4 +1,7 @@
 #!/bin/sh 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew install npm
+brew install bash-completion
+brew install git
+brew install node
 npm install -g bower
+npm install -g grunt-cli
